ac59c1f8574522328e4798ad34dd42b181170b46
HongzhenZhou <36949252+HongzhenZhou@users.noreply.github.com>
2022-12-05 15:11:11 -0800

    Add files via upload

--- python/tcn.py
+++ python/tcn.py
@@ -9,58 +9,59 @@ from darts import TimeSeries
 from darts.models import RNNModel
 from darts.models import TCNModel
 from darts.utils import timeseries_generation as tg
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 np.random.seed(42)
 
 parser = argparse.ArgumentParser(usage='Inquire the price for stock, need the stock ID argument')
 parser.add_argument('--sid', '-s', type = str, required = True, help = 'stock ID')
-parser.add_argument('--epochs', '-e', type = int, required = True, help = 'epochs')
+parser.add_argument('--epochs', '-e', type = int, help = 'epochs')
 args = parser.parse_args()
 sid = args.sid
-epochs = args.epochs
+epochs = args.epochs if args.epochs else 600
 
 if sid and len(sid) > 6:
-	yfd = yfsi.get_data(sid, start_date='01/01/2012')[5:]
+	yfd = yfsi.get_data(sid, start_date='02/11/2013')[20:]
 	if len(yfd) < 90:
 		print(f'Too short history array data for {sid}')
 		exit(1)
-	sdc = yfd[:-4]
+	hld = yfsi.get_data('CNH=F', start_date='02/11/2013')
+	hld = hld[hld.index.isin(yfd.index)]
+	yfd = yfd[yfd.index.isin(hld.index)]
+	sdc = yfd[:]
 	sdc['time'] = sdc.index
-	print(sdc['time'][-1])
 	del sdc['ticker']
 	sdc.index = pd.RangeIndex(0, len(sdc))
 	sdc = sdc.fillna(method = 'ffill')
+	hld.index = pd.RangeIndex(0, len(hld))
+	hld = hld.fillna(method = 'bfill')
 
 	pprice = sdc['adjclose']
+	pprice.name = 'price'
 	t = sdc['open'] - sdc['close']
 	pdiff = (t / t.max()) * 100
 	t = sdc['high'] - sdc['low']
 	pwidth = (t / t.max()) * 100
 	t = sdc['volume'] * ((sdc['close'] + sdc['open'] + sdc['high'] + sdc['low']) / 4)
 	pvol = (t / t.max()) * 10000
-	pprice.name = 'price'
 	pvol.name = 'vol'
+	prmb = (hld['adjclose'] / hld['adjclose'].max()) * 100
 
 	tsd = pd.DataFrame(pprice, columns = ['price'])
 	psd = pd.DataFrame(pvol, columns = ['vol'])
+	psd['rmb'] = prmb
 	psd['diff'] = pdiff
 	psd['width'] = pwidth
 	psd['time_index'] = psd.index
 	tsdd = TimeSeries.from_dataframe(tsd)
 	psdd = TimeSeries.from_dataframe(psd)
 
-	my_stopper = EarlyStopping(monitor = 'train_loss', patience = 10, min_delta = 0.05, mode = 'min',)
-
-	#m = TCNModel(14, 1, pl_trainer_kwargs = {"accelerator": "cpu", "callbacks": [my_stopper]})
-	#m = TCNModel(14, 1, pl_trainer_kwargs = {"accelerator": "cpu"})
 	m = TCNModel(30, 1)
 	m.fit(tsdd, epochs = epochs, past_covariates = [psdd])
 	p = m.predict(1, past_covariates = [psdd])
 	print('-----------------------')
-	print(f'>>>> {epochs} {yfd.index[-4]}')
+	print(f'>>>> {epochs} {yfd.index[-1]}')
 	print('+++++')
-	print(p[0][0][0])
+	print(p)
 	#tsd.plot()
 	#p.plot(label = 'predict')
 	#plt.show()
