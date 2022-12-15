123e6bb30064d235c70aa973837f637bbf2f4504
HongzhenZhou <36949252+HongzhenZhou@users.noreply.github.com>
2022-12-15 13:54:43 -0800

    Add files via upload

--- python/tcn.py
+++ python/tcn.py
@@ -14,22 +14,23 @@ random.seed(42)
 np.random.seed(42)
 torch.manual_seed(42)
 
-parser = argparse.ArgumentParser(usage='Inquire the price for an A share stock, need the stock ID argument')
+parser = argparse.ArgumentParser(usage='Inquire the price for stock, need the stock ID argument')
 parser.add_argument('--sid', '-s', type = str, required = True, help = 'stock ID')
 parser.add_argument('--epochs', '-e', type = int, help = 'epochs')
 parser.add_argument('-p', action='store_true')
 args = parser.parse_args()
 sid = args.sid
-epochs = args.epochs if args.epochs else 400
+epochs = args.epochs if args.epochs else 900
 
 if sid and len(sid) >= 6:
 	if len(sid) == 6:
-                sid += '.SZ' if int(sid[0]) < 6 else '.SS'
+		sid += '.SZ' if int(sid[0]) < 6 else '.SS'
 
 	yfd = yfsi.get_data(sid, start_date='02/11/2013')[20:]
 	if len(yfd) < 90:
 		print(f'Too short history array data for {sid}')
 		exit(1)
+	print(f'{sid} last close price on {yfd.index[-1]}: {round(yfd["close"][-1], 2)}')
 	hld = yfsi.get_data('CNH=F', start_date='02/11/2013')
 	hld = hld[hld.index.isin(yfd.index)]
 	yfd = yfd[yfd.index.isin(hld.index)]
@@ -60,16 +61,21 @@ if sid and len(sid) >= 6:
 	tsdd = TimeSeries.from_dataframe(tsd)
 	psdd = TimeSeries.from_dataframe(psd)
 
-	m = TCNModel(30, 1)
-	m.fit(tsdd, epochs = epochs, past_covariates = [psdd])
-	p = m.predict(1, past_covariates = [psdd])
+	train, val = tsdd[0 : -90], tsdd[-90 : ]
+	train_cov, val_cov = psdd[0 : -90], psdd[-90 : ]
+	m = TCNModel(30, 10, n_epochs = epochs, dropout = 0.1, dilation_base = 2, weight_norm = True, kernel_size = 5, num_filters = 3, random_state = 0)
+	#m.fit(tsdd, epochs = epochs, past_covariates = [psdd])
+	m.fit(series = train, past_covariates = train_cov, val_series = val, val_past_covariates = val_cov, verbose = False)
+	#p = m.historical_forecasts(series = tsdd, past_covariates = psdd, start = 0.1, forecast_horizon = 1, retrain = False, verbose = False)
+	p = m.predict(10, series = tsdd, past_covariates = psdd)
 	print('-----------------------')
 	print(f'>>>> {epochs} {yfd.index[-1]}')
 	print('+++++')
 	print(p)
 	if args.p:
 		tsd.plot()
-		p.plot(label = f'{sid} forecast the next close price after {yfd.index[-1]}')
+		p.plot(label = f'{sid} forecast the next close price after {yfd.index[-1]}, low_quantile=0.05, high_quantile=0.95')
+		plt.legend()
 		plt.show()
 	print('-----------------------')
 
